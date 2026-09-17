ef2fdafe345ba03ea58d92c54173ba4fadfde10f
Phil <damoqiongqiu@126.com>
2026-09-17 19:22:44 +0800

    docs(ice-web-components): 同步局部主题作用域指南 + 样板页 + 1.18.0 运行时

--- site-versions.js
+++ site-versions.js
@@ -9,7 +9,7 @@ module.exports = {
   // 应用层产品
   entityDesigner: '0.4.3',
   iceChart: '0.23.4',
-  iceWebComponents: '1.17.0',
+  iceWebComponents: '1.18.0',
   // DSL 兄弟包（ice-chart / ice-entity-designer 落地页与生态表引用）
   iceChartDsl: '0.2.9',
   entityDesignerDsl: '0.0.29',
